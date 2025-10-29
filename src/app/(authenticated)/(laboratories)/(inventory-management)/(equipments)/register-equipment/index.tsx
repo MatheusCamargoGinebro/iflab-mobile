a01cee705b581adcba2939297259528cf7d3b115
Camarg0vs <matheus.ginebro@aluno.ifsp.edu.br>
2025-10-29 09:31:44 -0300

    authenticated structure files done

---
+++ src/app/(authenticated)/(laboratories)/(inventory-management)/(equipments)/register-equipment/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function () {
+	return (
+		<View className="flex-1 gap-2 items-center justify-center">
+			<Text className="text-2xl font-bold text-white">Register equipment</Text>
+		</View>
+	);
+}
